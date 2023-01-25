f33c95318f1b1afb322664f699490fbecac0a8fa
powerjms0324 <powerjms0324@naver.com>
2023-01-25 09:15:43 +0000

    apptest change

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,7 +3,4 @@ import App from './App';
 import axios from 'axios';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
